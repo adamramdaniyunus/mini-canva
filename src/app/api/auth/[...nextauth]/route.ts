e558865101733c2820fbed3bb0a73338fbc8af62
adamramdaniyns <adam.ramdani@salamenterprise.com>
2025-04-11 13:28:50 +0700

    feat: add next auth setup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,5 @@
+import { authOptions } from "@/lib/nexauth";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (credentials?.email === "admin@gmail.com" && credentials?.password === "password") {
-          return { id: "1", name: "Admin User" };
-        }
-        return null;
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
 
 const handler = NextAuth(authOptions);
 
